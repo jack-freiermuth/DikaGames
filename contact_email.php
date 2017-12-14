e77ce7bb7b4c4ba8b2051c98ea6cf38e6dc3dc67
Jack Freiermuth <jack.freiermuth@gmail.com>
2017-12-13 20:02:34 -0600

    Message translation

--- contact_email.php
+++ contact_email.php
@@ -12,16 +12,16 @@ $body = "From: $name\nE-Mail: $email\nMessage:\n$message";
 
 if ( !$name || !$email || !$message ) {
 	$response_array['status'] = 'error'; 
-	$response_array['message'] = "You forgot to fill out a field."; 
+	$response_array['message'] = "Você esqueceu de preencher um campo."; 
 	$response_array['status_code'] = "1"; 
 } else {
 	if ('localhost' == $_SERVER['HTTP_HOST'] || mail ($to, $subject, $body, $from)) { 
 		$response_array['status'] = 'success';
-		$response_array['message'] = "<strong>Your message was sent!</strong> I'll get back to you as soon as I can!"; 
+		$response_array['message'] = "<strong>Sua mensagem foi enviada, entraremos em contato assim que possível"; 
 		$response_array['status_code'] = "0";
 	} else { 
 		$response_array['status'] = 'error'; 
-		$response_array['message'] = "Email didn't send."; 
+		$response_array['message'] = "Email não enviado."; 
 		$response_array['status_code'] = "2"; 
 	} 
 }
